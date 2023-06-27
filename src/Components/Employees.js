234ef03783012921d531a3d0126734d3586d9fb0
Rishav Kumar <102512758+rishavsrivastwa@users.noreply.github.com>
2023-06-27 11:31:22 +0530

    React Crud Basic

---
+++ src/Components/Employees.js
@@ -0,0 +1,19 @@
+const Employee = [
+    {
+        id:"1",
+        Name:"Manoj Kumar",
+        Age:"30"
+    },
+    {
+        id:"2",
+        Name:"MnIdeal",
+        Age:"23"
+    },
+    {
+        id:"3",
+        Name:"Ayush Raj",
+        Age:"37"
+    }
+]
+
+export default Employee;
